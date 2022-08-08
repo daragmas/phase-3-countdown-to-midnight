c4ce21bd805f08dec9e95a00accdaa38529a239f
daragmas <daragmas@gmail.com>
2022-08-08 17:29:49 -0400

    commit tree 5838b3d3dd082495523ac6eeb58d7c495e7c63b6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown int_arg
+    while int_arg > 0
+        puts "#{int_arg} SECOND(S)!"
+        int_arg-=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep int_arg
+        while int_arg > 0
+        sleep(1)
+        puts "#{int_arg} SECOND(S)!"
+        int_arg-=1
+    end
+    "HAPPY NEW YEAR!"
 end
